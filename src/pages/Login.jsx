37d7d57f63a1f2e4d248b6eeb65692d4fe05eb55
marinafischer <marinamirandafischer@gmail.com>
2022-03-08 14:54:59 -0300

    feat:valida inputs(5)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { PASSWORD_LENGTH, emailRegex } from '../helpers/constants';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    if (emailRegex.test(email) && password.length > PASSWORD_LENGTH) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [password, email]);
 
   return (
     <Form>
@@ -27,9 +37,9 @@ export default function Login() {
       <Button
         data-testid="login-submit-btn"
         type="submit"
+        disabled={ disabled }
       >
         Enter
-
       </Button>
     </Form>
   );
--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -4,20 +4,26 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
+const EMAIL_INPUT = 'email-input';
+const PASSWORD_INPUT = 'password-input';
+const BTN_SUBMIT = 'login-submit-btn';
+const TEST_EMAIL = 'teste@teste.com';
+const TEST_PASSWORD = '1234567';
+
 describe('2 - Crie todos para a tela de login', () => {
   it('O input de email deve possuir o atributo data-testid="email-input"', () => {
     renderWithRouter(<App />);
-    const emailInput = screen.getByTestId('email-input');
+    const emailInput = screen.getByTestId(EMAIL_INPUT);
     expect(emailInput).toBeInTheDocument();
   });
   it('O input de senha deve possuir o atributo data-testid="password-input"', () => {
     renderWithRouter(<App />);
-    const passwordInput = screen.getByTestId('password-input');
+    const passwordInput = screen.getByTestId(PASSWORD_INPUT);
     expect(passwordInput).toBeInTheDocument();
   });
   it('O botão "Enter" deve possuir o atributo data-testid="login-submit-btn"', () => {
     renderWithRouter(<App />);
-    const btnSubmit = screen.getByTestId('login-submit-btn');
+    const btnSubmit = screen.getByTestId(BTN_SUBMIT);
     expect(btnSubmit).toBeInTheDocument();
   });
 });
@@ -25,17 +31,66 @@ describe('2 - Crie todos para a tela de login', () => {
 describe('3 - A pessoa deve conseguir escrever seu email no input de email', () => {
   it('É possível escrever o email', () => {
     renderWithRouter(<App />);
-    const emailInput = screen.getByTestId('email-input');
-    userEvent.type(emailInput, 'teste@teste.com');
-    expect(emailInput).toHaveValue('teste@teste.com');
+    const emailInput = screen.getByTestId(EMAIL_INPUT);
+    userEvent.type(emailInput, TEST_EMAIL);
+    expect(emailInput).toHaveValue(TEST_EMAIL);
   });
 });
 
 describe('4 - A pessoa deve conseguir escrever seu email no input de email', () => {
   it('É possível escrever a senha', () => {
     renderWithRouter(<App />);
-    const passwordInput = screen.getByTestId('password-input');
-    userEvent.type(passwordInput, '1234567');
-    expect(passwordInput).toHaveValue('1234567');
+    const passwordInput = screen.getByTestId(PASSWORD_INPUT);
+    userEvent.type(passwordInput, TEST_PASSWORD);
+    expect(passwordInput).toHaveValue(TEST_PASSWORD);
+  });
+});
+
+describe('5 - botão só é liberado com e-mail e senha válido', () => {
+  it('botão está desabilitado', () => {
+    renderWithRouter(<App />);
+    const btnSubmit = screen.getByTestId(BTN_SUBMIT);
+    expect(btnSubmit).toHaveAttribute('disabled');
+  });
+  it('botão está desabilitado só com email valido', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getByTestId(EMAIL_INPUT);
+    const btnSubmit = screen.getByTestId(BTN_SUBMIT);
+    userEvent.type(emailInput, TEST_EMAIL);
+    expect(btnSubmit).toHaveAttribute('disabled');
+  });
+  it('botão está desabilitado só com senha valido', () => {
+    renderWithRouter(<App />);
+    const passwordInput = screen.getByTestId(PASSWORD_INPUT);
+    const btnSubmit = screen.getByTestId(BTN_SUBMIT);
+    userEvent.type(passwordInput, TEST_PASSWORD);
+    expect(btnSubmit).toHaveAttribute('disabled');
+  });
+  it('botão está abilitado com e-mail e senha valido', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getByTestId(EMAIL_INPUT);
+    const passwordInput = screen.getByTestId(PASSWORD_INPUT);
+    const btnSubmit = screen.getByTestId(BTN_SUBMIT);
+    userEvent.type(emailInput, TEST_EMAIL);
+    userEvent.type(passwordInput, TEST_PASSWORD);
+    expect(btnSubmit).not.toHaveAttribute('disabled');
+  });
+  it('botão está desabilitado com e-mail invalido e senha valida', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getByTestId(EMAIL_INPUT);
+    const passwordInput = screen.getByTestId(PASSWORD_INPUT);
+    const btnSubmit = screen.getByTestId(BTN_SUBMIT);
+    userEvent.type(emailInput, 'textoErrado.com');
+    userEvent.type(passwordInput, TEST_PASSWORD);
+    expect(btnSubmit).toHaveAttribute('disabled');
+  });
+  it('botão está desabilitado com e-mail valido e senha invalida', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getByTestId(EMAIL_INPUT);
+    const passwordInput = screen.getByTestId(PASSWORD_INPUT);
+    const btnSubmit = screen.getByTestId(BTN_SUBMIT);
+    userEvent.type(emailInput, TEST_EMAIL);
+    userEvent.type(passwordInput, '123456');
+    expect(btnSubmit).toHaveAttribute('disabled');
   });
 });
