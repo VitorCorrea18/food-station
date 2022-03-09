7f18efceb0892d09d59cd82a57b0de3617fe1030
marinafischer <marinamirandafischer@gmail.com>
2022-03-09 18:59:16 -0300

    feat:renderiza elementos na tela(33)

--- src/pages/FoodRecipe.jsx
+++ src/pages/FoodRecipe.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 export default function FoodRecipe() {
   return (
-    <div>FoodRecipe</div>
+    <div>
+      <img data-testid="recipe-photo" />
+      <h1 data-testid="recipe-title">TESTE</h1>
+      <button data-testid="share-btn">SHARE</button>
+      <button data-testid="favorite-btn">FAVORITE</button>
+      <p data-testid="recipe-category">
+        texto da categoria
+      </p>
+      <ul>
+        <li data-testid={ `${0}-ingredient-name-and-measure` }> carninha </li>
+      </ul>
+      <p data-testid="instructions">
+        explicação
+      </p>
+      <iframe data-testid="video" />
+      <div data-testid={ `${0}-recomendation-card` }>RECOMENDADA</div>
+      <button data-testid="start-recipe-btn">INICIAR RECEITA</button>
+    </div>
   );
 }
