8110dcb6af6aba1dd83e592c5c35a82605e81e86
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2022-03-10 16:05:56 -0300

    feat: add explore foods and drinks(72)/ add redirect on buttons

--- src/pages/ExploreDrink.jsx
+++ src/pages/ExploreDrink.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExploreDrinks() {
+  const history = useHistory();
   return (
     <>
       <Header title="Explore Drinks" withSearchButton={ false } />
       <button
         type="button"
         data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/drinks/ingredients') }
       >
         By Ingredient
       </button>
--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function ExploreFoods() {
+  const history = useHistory();
   return (
     <>
       <Header title="Explore Foods" withSearchButton={ false } />
       <button
         type="button"
         data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
       >
         By Ingredient
       </button>
