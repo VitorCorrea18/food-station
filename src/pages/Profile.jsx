003860dbd24fe3e0ce00b35e100ff3018911495b
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2022-03-10 20:33:38 -0300

    feat: add profile(82)/ add data-testid

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,7 +6,25 @@ export default function Profile() {
   return (
     <>
       <Header title="Profile" withSearchButton={ false } />
-      <h1>Profile</h1>
+      <h2 data-testid="profile-email"> </h2>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+      >
+        Logout
+      </button>
       <Footer />
     </>
 
