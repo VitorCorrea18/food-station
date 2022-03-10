2aa331a011165631f3b60513cf452b783489035d
vitorcorrea18 <vitorcorrea18@gmail.com>
2022-03-09 20:53:46 -0400

    feat: criando as funçoes de fetch e preparando o componente CategoryButton para os filtros(28)

--- src/components/CategoryButtons.jsx
+++ src/components/CategoryButtons.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import AppContext from '../context/context';
 
 export default function CategoryButtons({ category }) {
+  const [foodData, setFoodData] = useContext(AppContext);
+
   return (
     <button
       type="button"
@@ -9,6 +12,12 @@ export default function CategoryButtons({ category }) {
     >
       { category }
     </button>
+    // Os requisito 28 e 29 podem ser feitos por aqui, segue o meu raciocionio até o momento:
+    // Manipular os contextos por aqui "foodData", "setFoodData".
+    // Trocar esse button por um toggle do bootstrap igual foi feito no searchBar,
+    // e adicionar a lógica de ativar e desativar o filtro chamando a Api correspondente em cada caso.
+    // Obs: acho que da pra usar a callInicialFetch nos helpers para retirar o filtro.
+    // As funções de fetchByCategory estão na pasta services já prontas.
   );
 }
 
