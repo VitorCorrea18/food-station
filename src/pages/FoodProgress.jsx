7cfe40f6c0e36371b9cb9f0d073da7d5c722ff68
marinafischer <marinamirandafischer@gmail.com>
2022-03-11 17:22:52 -0300

    feat:implementa tela de processo de comida(47)

--- src/pages/FoodProgress.jsx
+++ src/pages/FoodProgress.jsx
@@ -1,7 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import fetchFoodRecipe from '../services/fetchFoodRecipe';
+import getIngredientesMeasure from '../helpers/getFoodIngrMeasure';
+import { isFavorite } from '../helpers/setFavorite';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+
+const copy = require('clipboard-copy');
 
 export default function FoodProgress() {
-  return (
-    <div>FoodProgress</div>
-  );
+  const [recipe, setRecipe] = useState({});
+  const [ingrMeasure, setIngrMeasure] = useState({});
+  const [copyMessage, setCopyMessage] = useState('');
+  const [favorite, setFavorite] = useState(false);
+  const history = useHistory();
+  const { location: { pathname } } = history;
+
+  const getData = async (id) => {
+    const data = await fetchFoodRecipe(id);
+    setRecipe(data);
+    getIngredientesMeasure(data, setIngrMeasure);
+  };
+
+  useEffect(() => {
+    const id = pathname.split('/')[2];
+    getData(id);
+    isFavorite(id, setFavorite);
+  }, [pathname]);
+
+  const share = () => {
+    copy(`http://localhost:3000${pathname}`);
+    setCopyMessage('Link copied!');
+  };
+
+  if (Object.keys(recipe).length > 0) {
+    return (
+      <div>
+        <img data-testid="recipe-photo" alt="food" src={ recipe.strMealThumb } />
+        <h1 data-testid="recipe-title">{recipe.strMeal}</h1>
+        {copyMessage.length > 0 && (
+          <p>{copyMessage}</p>
+        )}
+        <button
+          onClick={ share }
+          data-testid="share-btn"
+          type="button"
+        >
+          <img src={ shareIcon } alt="share icon" />
+        </button>
+        <button
+          type="button"
+          onClick={ () => { handleFavoriteMeal(favorite, setFavorite, recipe); } }
+        >
+          <img
+            data-testid="favorite-btn"
+            src={ favorite ? blackHeartIcon : whiteHeartIcon }
+            alt="favorite icon"
+          />
+        </button>
+        <p data-testid="recipe-category">
+          {recipe.strCategory}
+        </p>
+        <ul>
+          { Object.keys(ingrMeasure).length > 0
+          && ingrMeasure.filterIngridients.map((ingredient, index) => (
+            <li
+              key={ index }
+              data-testid={ `${index}-ingredient-step` }
+            >
+              {`${ingredient} - ${ingrMeasure.filterMeasures[index]}`}
+            </li>
+          ))}
+        </ul>
+        <p data-testid="instructions">
+          {recipe.strInstructions}
+        </p>
+        <button
+          className="fixed-bottom border"
+          data-testid="finish-recipe-btn"
+          type="button"
+          // onClick={ }
+        >
+          Finish
+        </button>
+      </div>
+    );
+  }
+  return (<div>loading...</div>);
 }
