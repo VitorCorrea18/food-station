3edf3531358c42d87700a9d27b040df66e561569
marinafischer <marinamirandafischer@gmail.com>
2022-03-12 18:31:27 -0300

    feat:direciona para a tela principal filtrando pelo ingrediente(77)

--- src/pages/ExploreDrinkIng.jsx
+++ src/pages/ExploreDrinkIng.jsx
@@ -1,10 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { fetchDrinkIngreList } from '../services/fetchIngredients';
+import fetchData from '../services/fetchData';
+import AppContext from '../context/context';
+import { FIRST_TWELVE } from '../helpers/constants';
 
 export default function ExploreDrinksIng() {
+  const { setDrinkData } = useContext(AppContext);
   const [data, setData] = useState([]);
+  const history = useHistory();
+
+  const handleExplore = async (ingrediente) => {
+    const searchIngr = await fetchData(ingrediente, 'i', '/drinks');
+    setDrinkData(searchIngr.slice(0, FIRST_TWELVE));
+    history.push('/drinks');
+  };
 
   const getData = async () => {
     const ingrData = await fetchDrinkIngreList();
@@ -19,8 +31,12 @@ export default function ExploreDrinksIng() {
       {
         data.map((ingrediente, index) => (
           <div
-            key={ ingrediente.idIngredient }
+            key={ ingrediente.strIngredient1 }
             data-testid={ `${index}-ingredient-card` }
+            onClick={ () => { handleExplore(ingrediente.strIngredient1); } }
+            onKeyDown={ () => { handleExplore(ingrediente.strIngredient1); } }
+            role="button"
+            tabIndex={ 0 }
           >
             <h1 data-testid={ `${index}-card-name` }>
               {ingrediente.strIngredient1}
--- src/pages/ExploreFoodIng.jsx
+++ src/pages/ExploreFoodIng.jsx
@@ -1,10 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { fetchFoodIngreList } from '../services/fetchIngredients';
+import fetchData from '../services/fetchData';
+import AppContext from '../context/context';
+import { FIRST_TWELVE } from '../helpers/constants';
 
 export default function ExploreFoodsIng() {
+  const { setFoodData } = useContext(AppContext);
   const [data, setData] = useState([]);
+  const history = useHistory();
+
+  const handleExplore = async (ingrediente) => {
+    const searchIngr = await fetchData(ingrediente, 'i', '/foods');
+    setFoodData(searchIngr.slice(0, FIRST_TWELVE));
+    history.push('/foods');
+  };
 
   const getData = async () => {
     const ingrData = await fetchFoodIngreList();
@@ -22,6 +34,10 @@ export default function ExploreFoodsIng() {
           <div
             key={ ingrediente.idIngredient }
             data-testid={ `${index}-ingredient-card` }
+            onClick={ () => { handleExplore(ingrediente.strIngredient); } }
+            onKeyDown={ () => { handleExplore(ingrediente.strIngredient); } }
+            role="button"
+            tabIndex={ 0 }
           >
             <h1 data-testid={ `${index}-card-name` }>
               {ingrediente.strIngredient}
