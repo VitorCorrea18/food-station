860165ac5a8a57b15df2eb8d7cc77aba7ee5e1ed
marinafischer <marinamirandafischer@gmail.com>
2022-03-12 17:55:29 -0300

    feat:cria elementos da tela explorar ingre(75)

--- src/pages/ExploreDrinkIng.jsx
+++ src/pages/ExploreDrinkIng.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { fetchDrinkIngreList } from '../services/fetchIngredients';
 
 export default function ExploreDrinksIng() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const ingrData = await fetchDrinkIngreList();
+    setData(ingrData);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
       <Header title="Explore Ingredients" withSearchButton={ false } />
-      <h1>Explore Drinks Ingredients</h1>
+      {
+        data.map((ingrediente, index) => (
+          <div
+            key={ ingrediente.idIngredient }
+            data-testid={ `${index}-ingredient-card` }
+          >
+            <h1 data-testid={ `${index}-card-name` }>
+              {ingrediente.strIngredient1}
+            </h1>
+            <img
+              alt="ingrediente"
+              src={ `https://www.themealdb.com/images/ingredients/${ingrediente.strIngredient1}.png` }
+              data-testid={ `${index}-card-img` }
+            />
+          </div>
+        ))
+      }
       <Footer />
     </>
   );
