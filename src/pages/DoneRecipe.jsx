54a33a5d8c6b0cb8302aa030f7feb1c63e0cdb88
marinafischer <marinamirandafischer@gmail.com>
2022-03-15 09:36:28 -0300

    feat:implementa botoes de filtro(58)

--- src/pages/DoneRecipe.jsx
+++ src/pages/DoneRecipe.jsx
@@ -4,19 +4,44 @@ import DoneCard from '../components/DoneCard';
 
 export default function DoneRecipe() {
   const [data, setData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
 
   useEffect(() => {
     const doneRecipe = JSON.parse(localStorage.getItem('doneRecipes')) || [];
     setData(doneRecipe);
+    setFilterData(doneRecipe);
   }, []);
+
+  const handleFilter = (type) => {
+    const filter = filterData.filter((item) => item.type === type);
+    return ((type === 'all') ? setData(filterData) : setData(filter));
+  };
 
   return (
     <>
       <Header title="Done Recipes" withSearchButton={ false } />
       <div>
-        <button data-testid="filter-by-all-btn" type="button">All</button>
-        <button data-testid="filter-by-food-btn" type="button">Food</button>
-        <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          onClick={ () => handleFilter('all') }
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+          onClick={ () => handleFilter('food') }
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+          onClick={ () => handleFilter('drink') }
+        >
+          Drinks
+        </button>
       </div>
       <div>
         {data.map((recipe, index) => (
