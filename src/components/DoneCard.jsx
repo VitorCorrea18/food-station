77eb02ca4fc3426427011ea7ed8be975cd0bf223
marinafischer <marinamirandafischer@gmail.com>
2022-03-21 12:16:16 -0300

    subir barnch

--- src/components/DoneCard.jsx
+++ src/components/DoneCard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes, { string } from 'prop-types';
 import shareIcon from '../images/shareIcon.svg';
+import '../styles/doneRecipe.css';
 
 const copy = require('clipboard-copy');
 
@@ -18,7 +19,7 @@ export default function DoneCard({ data, index }) {
   };
 
   return (
-    <div>
+    <div className="main-card">
       <div
         onClick={ () => redirect() }
         onKeyDown={ () => redirect() }
@@ -32,40 +33,49 @@ export default function DoneCard({ data, index }) {
           alt="recipe"
         />
       </div>
-      <h3 data-testid={ `${index}-horizontal-top-text` }>
-        {`
+      <div>
+
+        <h3 data-testid={ `${index}-horizontal-top-text` }>
+          {`
           ${data.type === 'food' ? data.nationality : data.alcoholicOrNot}
           - ${data.category}
         `}
-      </h3>
-      <div
-        onClick={ () => redirect() }
-        onKeyDown={ () => redirect() }
-        role="button"
-        tabIndex={ 0 }
-      >
-        <h2 data-testid={ `${index}-horizontal-name` }>{data.name}</h2>
-      </div>
-      <h4 data-testid={ `${index}-horizontal-done-date` }>
-        {`Done in: ${data.doneDate}`}
-      </h4>
-      {copyMessage.length > 0 && (
-        <p>{copyMessage}</p>
-      )}
-      <button
-        data-testid={ `${index}-horizontal-share-btn` }
-        type="button"
-        onClick={ share }
-        src={ shareIcon }
-      >
-        <img src={ shareIcon } alt="share icon" />
-      </button>
-      <div>
-        {data.tags.map((tag, i) => (
-          i < 2 && (
-            <span key={ i } data-testid={ `${index}-${tag}-horizontal-tag` }>{tag}</span>
-          )
-        ))}
+        </h3>
+        <div
+          onClick={ () => redirect() }
+          onKeyDown={ () => redirect() }
+          role="button"
+          tabIndex={ 0 }
+        >
+          <h2 data-testid={ `${index}-horizontal-name` }>{data.name}</h2>
+        </div>
+        <h4 data-testid={ `${index}-horizontal-done-date` }>
+          {`Done in: ${data.doneDate}`}
+        </h4>
+        {copyMessage.length > 0 && (
+          <p>{copyMessage}</p>
+        )}
+        <button
+          data-testid={ `${index}-horizontal-share-btn` }
+          type="button"
+          onClick={ share }
+          src={ shareIcon }
+        >
+          <img src={ shareIcon } alt="share icon" />
+        </button>
+        <div>
+          {data.tags.map((tag, i) => (
+            i < 2 && (
+              <span
+                key={ i }
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+              >
+                {tag}
+
+              </span>
+            )
+          ))}
+        </div>
       </div>
     </div>
   );
